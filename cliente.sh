2ecfcd6b65a2ab8a992d588a30aeb581430396ac
lax27 <alex.taravilla@enti.cat>
2022-11-09 11:12:15 +0100

    comprobaciones y vacas

--- cliente.sh
+++ cliente.sh
@@ -1,6 +1,54 @@
 #!/bin/bash
+PORT="2223"
+SERVER_AD="localhost"
+
 echo "cliente TURIP"
 
 echo "(1) SEND_MSG: HOLI_TURIP"
-echo "HOLI_TURIP: 127.0.0.1" | nc localhost 2223
+
+echo "HOLI_TURIP 127.0.0.1" | nc $SERVER_AD $PORT
+
+echo "(2) LISENT: comprobacion handsake"
+
+MSG=`nc -l $PORT`
+
+echo $MSG
+
+if [ "$MSG" != "OK_TURIP" ]
+then
+	echo "ERROR 1: Handshake incorecto"
+	exit 1
+fi
+
+echo "(5) SEND"
+echo "echo "FILE_NAME vaca.vaca" " | nc $SERVER_AD $PORT 
+
+echo "(6) LISENT"
+MSG=`nc -l $PORT`
+
+if [ "$MSG" != "OK_FILE_NAME" ]
+then
+
+echo "ERROR 2: Prefix FILE_NAME NOT FOUND"
+exit 2
+fi
+
+
+
+
+
+
+
+
+
+
+exit 0
+
+
+
+
+
+
+
+
 
--- servidor.sh
+++ servidor.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
+PORT="2223"
+
 echo "servidor Transfer Unit Recursive International Protocol: TURIP"
 
-echo "(0) LISTEN"
-MSG=`nc -l 2223`
+echo "(0) LISTEN: handshake "
+MSG=`nc -l $PORT`
+HANDSHAKE=`echo $MSG | cut -d " " -f 1`
+IP_CLIENT=`echo $MSG | cut -d " " -f 2`
 
-if [ "$MSG" != "HOLI_TURIP" ]
+if [ "$HANDSHAKE" != "HOLI_TURIP" ]
 then
 	echo "ERROR 1: handshake incorecto"
-	exit 1
+	
+	echo "KO_TURIP" | nc $IP_CLIENT $PORT
+
+	exit 1	
 fi
+
+echo "OK_TURIP" | nc $IP_CLIENT $PORT
+
+echo "(4) LISENT"
+MSG=`nc -l $PORT`
+PREFIX=`$MSG | cut -d " " -f 1`
+FILE=`$MSG | cut -d " " -f 2`
+
+if [ "$PREFIX" != "FILE_NAME" ]
+then
+
+echo "ERROR 2: prefix not found"
+echo "KO_FILE_NAME" | nc $IP_CLIENT $PORT
+exit 2
+
+fi
+
+echo "OK_FILE_NAME" | nc $IP_CLIENT $PORT
+
+
+exit 0
+
 
 
 
